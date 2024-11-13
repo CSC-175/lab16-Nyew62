a8f5e2b3dd43b71eb5361ca5cb4f1b9af759e368
nyew@umich.edu <nyew@umich.edu>
2024-11-13 08:58:06 -0500

    Attempt 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,12 @@
 // Code to implement the gcf function goes here
+int gcf(int a, int b) {
+    if (a%b == 0) {
+        return b;
+    }
+    else {
+        return gcf(b, a % b);
+    }
+
+}
+
+//lab16_Nyew62 Tests/test1.cpp lab16Driver.cpp)
